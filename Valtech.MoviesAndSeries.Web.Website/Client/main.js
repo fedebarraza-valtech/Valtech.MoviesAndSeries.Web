cd79650fc60a2a339dc2fb4a3009144184dba6bc
Federico Barraza <federico.barraza@valtech.com>
2020-09-22 21:21:29 -0300

    new client folder

---
+++ Valtech.MoviesAndSeries.Web.Website/Client/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import './scss/main.scss';
+
+//import router from '@/Scripts/router';
+//import store from '@/Scripts/store';
+//import { sync } from 'vuex-router-sync';
+//sync(store, router);
+
+/* eslint-disable-next-line no-unused-vars */
+let App = new Vue({
+    el: '#App',
+    //store,
+    //router
+});
---
+++ Valtech.MoviesAndSeries.Web.Website/Client/webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path'),
+    webpack = require('webpack');
+
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    mode: 'development',
+    entry: {
+        main: './main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../Static/'),
+        filename: "js/[name].min.js",
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new VueLoaderPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: 'css/[name].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader'
+                }]
+            },
+            {
+                test: /\.vue$/,
+                exclude: [/node_modules/],
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.(js|vue)$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    configFile: '.eslintrc.js'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                    {
+                        loader: 'style-resources-loader', options: {
+                            patterns: [
+                                path.resolve(__dirname, '../scss/resources.scss'),
+                            ],
+                        }
+                    },
+                ]
+            },
+        ]
+    },
+    stats: {
+        children: false,
+        colors: true
+    },
+    devtool: 'source-map'
+};
+
+module.exports = config;
